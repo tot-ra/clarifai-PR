fc5e1170519023f4e2fef3c25061c653ed26c76b
Artjom Kurapov <artjom.kurapov@clarifai.com>
2023-09-12 23:31:17 +0300

    ...

--- index.js
+++ index.js
@@ -80,19 +80,8 @@ async function reviewPR() {
         return
     }
 
-    return
 
     try {
-        // `who-to-greet` input defined in action metadata file
-        // const nameToGreet = core.getInput('who-to-greet');
-        // console.log(`Hello ${nameToGreet}!`);
-        // const time = (new Date()).toTimeString();
-        // core.setOutput("time", time);
-        // // Get the JSON webhook payload for the event that triggered the workflow
-        // const payload = JSON.stringify(github.context.payload, undefined, 2)
-        // console.log(`The event payload: ${payload}`);
-
-
         const PAT = process.env.CLARIFAI_PAT;
         const USER_ID = process.env.CLARIFAI_USER_ID;
         const APP_ID = process.env.CLARIFAI_APP_ID;
