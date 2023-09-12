8e09094ea86dc50a8e4e95c0b013b913fe5a90f7
Artjom Kurapov <artjom.kurapov@clarifai.com>
2023-09-13 01:39:52 +0300

    ...

--- dist/index.js
+++ dist/index.js
@@ -11032,7 +11032,7 @@ async function reviewPR() {
 
         for(let msg of data.repository.pullRequest.commits.edges[0].node.commit.tree.entries){
             if (msg.object?.text) {
-                RAW_TEXT += `\nFile "${msg.path}" contents: \n\n ${msg.object.text.substring(0, 30000)}`
+                RAW_TEXT += `\nFile "${msg.path}" contents: \n\n ${msg.object.text.substring(0, 1000)}`
             }
         }
 
